0bbd82aa9234d462df0be18db9dcdeca2656c1ef
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2017-01-31 15:19:06 +0000

    basic competition page

--- js/script.js
+++ js/script.js
@@ -31,20 +31,6 @@
 
 
 ///////////////////////////////////////
-//        Navigation
-///////////////////////////////////////
-
-  // mobile nav toggle open & close
-  $('.js-toggle-mobile-nav').on('click', function(e) {
-    $('.mobile-nav').toggleClass('is-open').toggleClass('is-closed');
-  });
-
-  // current page nav highlight
-  var currentPage = $('body').data('current-page');
-  $('.' + currentPage + ' .site-nav__item--' + currentPage).addClass('site-nav__item--current');
-
-
-///////////////////////////////////////
 //      SVG image swap
 ///////////////////////////////////////
 
@@ -57,78 +43,6 @@
       $(this).attr('src', newSrc);
     });
   }
-
-
-///////////////////////////////////////
-//      Parallax
-//      [ example: <div class="parallax" data-parallax-speed="0.2"> ]
-///////////////////////////////////////
-
-  // $(document).scroll(function(){
-  //   var scrolled = $(document).scrollTop();
-  //   $('.parallax').each(function(){
-  //     var speed = $(this).attr('data-parallax-speed');
-  //     var offset = $(this).offset();
-  //     var parallax = -(scrolled - offset.top) * speed ;
-  //     $(this).css('background-position', 'center ' + parallax + 'px');
-  //   });
-  // });
-
-
-///////////////////////////////////////
-//    Generic modal
-///////////////////////////////////////
-
-  var modal          = $('.js-modal'),
-      modalLaunchBtn = $('.js-open-modal'),
-      modalCloseBtn  = $('.js-close-modal');
-
-    // opens modal
-    function modalOpen(event){
-      event.preventDefault();
-      // disable scrolling on background content (doesn't work iOS)
-      $('body').addClass('disable-scroll');
-      // // open modal
-      modal.fadeIn('250', function(){
-        $(this).removeClass('is-closed').addClass('is-open');
-      });
-    }
-
-    // closes modal
-    function modalClose(event){
-      event.preventDefault();
-      // enable scrolling
-      $('body').removeClass('disable-scroll');
-      // close modal with fade
-      modal.fadeOut('250', function(){
-        $(this).removeClass('is-open').addClass('is-closed');
-      });
-    }
-
-    // launches modal when offer is clicked
-    modalLaunchBtn.on('click', function(event) {
-      modalOpen(event);
-    });
-
-    // closes modal on close icon click
-    modalCloseBtn.on('click', function(event) {
-      modalClose(event);
-    });
-
-    // closes modal on background click
-    modal.on('click', function(event) {
-      if (event.target !== this){
-        return;
-      }
-      modalClose(event);
-    });
-
-    // closes modal on escape key press
-    $(document).keyup(function(event) {
-       if (event.keyCode == 27) {
-         modalClose(event);
-        }
-    });
 
 
 ///////////////////////////////////////////////////////////////////////////////
